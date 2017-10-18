259f170ba268b4675e225839f95e08b6d8b88299
poonwz <e0031223@u.nus.edu>
2017-10-19 03:32:49 +0800

    Reduce line length for checkstyle

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -16,7 +16,8 @@ public class FindCommand extends Command {
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";*/
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names or phones "
-            + "contain any of the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "contain any of the specified keywords (case-insensitive) "
+            + "and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie 9876\n";
 
