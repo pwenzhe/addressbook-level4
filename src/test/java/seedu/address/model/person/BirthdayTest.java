c293d1b0e00b07a7ddf7162454c619bb0d9ec3e9
johnweikangong <ongwk95@hotmail.com>
2017-10-28 20:50:56 +0800

    PostalCodeTest: Add new tests for postal code

--- src/test/java/seedu/address/model/person/BirthdayTest.java
+++ src/test/java/seedu/address/model/person/BirthdayTest.java
@@ -10,15 +10,15 @@ public class BirthdayTest {
     @Test
     public void isValidBirthday() {
         // invalid birthday
-        assertFalse(Birthday.isValidBirthday("")); // empty string
-        assertFalse(Birthday.isValidBirthday("9188")); // less than 6 numbers
-        assertFalse(Birthday.isValidBirthday("birthday")); // invalid date
-        assertFalse(Birthday.isValidBirthday("9011p1")); // alphabets within digits
-        assertFalse(Birthday.isValidBirthday("931 534")); // spaces within digits
+        assertFalse(Birthday.isValidBirthday("")); // Empty string
+        assertFalse(Birthday.isValidBirthday("9188")); // Less than 6 numbers
+        assertFalse(Birthday.isValidBirthday("birthday")); // Invalid date
+        assertFalse(Birthday.isValidBirthday("9011p1")); // Alphabets within digits
+        assertFalse(Birthday.isValidBirthday("931 534")); // Spaces within digits
 
         // valid birthday
-        assertTrue(Birthday.isValidBirthday("12 12 87")); // can enter year using 2 digit
-        assertTrue(Birthday.isValidBirthday("14-05-1997")); // accepts '-' as separator
-        assertTrue(Birthday.isValidBirthday("08 aug 1985")); // name of month
+        assertTrue(Birthday.isValidBirthday("12 12 87")); // Can enter year using 2 digit
+        assertTrue(Birthday.isValidBirthday("14-05-1997")); // Accepts '-' as separator
+        assertTrue(Birthday.isValidBirthday("08 aug 1985")); // Name of month
     }
 }
