82179054e7443128b0c01ceffc27d3eec574ccae
poonwz <e0031223@u.nus.edu>
2017-11-13 14:25:03 +0800

    Add other matches for finding favourite

--- src/main/java/seedu/address/model/person/exceptions/PersonNotFoundException.java
+++ src/main/java/seedu/address/model/person/exceptions/PersonNotFoundException.java
@@ -1,6 +1,11 @@
 package seedu.address.model.person.exceptions;
 
+// @@author pwenzhe
 /**
  * Signals that the operation is unable to find the specified person.
  */
-public class PersonNotFoundException extends Exception {}
+public class PersonNotFoundException extends Exception {
+    public PersonNotFoundException() {
+        super("Operation could not find the specified person");
+    }
+}
